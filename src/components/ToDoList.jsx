b76f5a8a198617e51034e33e83e7da5d9d36aa2c
aritra995 <aritrasiliguri@gmail.com>
2024-05-02 17:45:41 +0530

    added Header, TodoList component

---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,11 @@
+const ToDoList = () => {
+    return (
+        <>
+            <div className="list">
+                Nothing to do buddy. Sleep!!
+            </div>
+        </>
+    );
+}
+
+export default ToDoList;
